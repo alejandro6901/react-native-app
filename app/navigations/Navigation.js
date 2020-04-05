26df54bd1de6b92b67ad87ac128b04f428b7042d
Ale <alejandro960137@gmail.com>
2020-04-04 22:59:21 -0600

    Navigation Tabs and Snacks

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import {Icon} from 'react-native-elements';
-import {createAppContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import CreateStacks from "./Stacks";
 
-import RestaurantsScreenStacks from './RestaurantsStacks';
+const Tab = createBottomTabNavigator();
 
-const NavigationStacks = createBottomTabNavigator({
-  Restaurants: {
-    screen: RestaurantsScreenStacks,
-    navigationOptions: () => ({
-      tabBarLabel: 'Restaurants',
-      tabBarIcon: ({tintColor}) => {
-        <Icon
-          type="material-community"
-          name="compass-outline"
-          size={22}
-          color={tintColor}
-        />;
-      },
-    }),
-  },
-});
+import RestaurantsScreen from "../screens/Restaurants";
+import TopRestaurantsScreen from "../screens/TopRestaurants";
+import MyAccountScreen from "../screens/MyAccount";
+import SearchScreen from "../screens/Search";
 
-export default createAppContainer(NavigationStacks);
+function RestaurantStack() {
+  return (<CreateStacks name="Restaurantes" component={RestaurantsScreen} />);
+}
+
+function TopRestaurantsStacks() {
+  return (<CreateStacks name="Top Restaurantes" component={TopRestaurantsScreen} />);
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurantes"
+          component={RestaurantStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Top 5"
+          component={TopRestaurantsStacks}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export default function TopRestaurants() {
   return (
     <View>
-      <Text>TopRestaurants</Text>
+      <Text>Top Restaurants</Text>
     </View>
   );
 }
